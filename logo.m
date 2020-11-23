09eeecfb631c9d3e7bfb81e5acc7e6c0acd759e8
Dan Rodman <dlrodman@gmail.com>
2020-11-23 09:54:33 -0800

    added logo display file

---
+++ logo.m
@@ -0,0 +1,12 @@
+% fancy logo on startup because I can
+clear;
+clc;                        
+display(" ___             _____ _____ _____ ");
+display("|  _|___ ___ ___|  _  | __  |     |");
+display("|  _| . |  _| .'|   __|    -|  |  |");
+display("|_| |___|___|__,|__|  |__|__|_____|");
+                                   
+fprintf('\n');
+
+fprintf('Dan Rodman 2020 \nReleased under Creative Commons CC0 1.0 Universal License \n');
+fprintf('No Rights Reserved \nAll Your Base Are Belong To Us \n');
